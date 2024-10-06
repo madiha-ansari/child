dae8a9b026c205ab8bc5e573fe57c8f1709d8f7a
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-10-06 09:34:25 +0500

    destructing

---
+++ arr.js
@@ -0,0 +1,27 @@
+
+    // var name = "Madiha";
+// var fName = "M. Shafiq";
+// console.log(`My name is ${name} & my ${fName} `);
+
+// destructing
+// arrary
+// const arrary = ["red","pink"];
+// let [value1,value2]=arrary;
+// console.log(`${value1} ${value2}`);
+
+// , skip
+// const arrary = ["red","pink","yellow"];
+// let [value1,,value2]=arrary;
+// console.log(`${value1} ${value2}`);
+
+// nested
+// const arrary = [["one", "two"], "three"];
+// let [[value1, value2], value3] = arrary;
+// console.log(value1, value2, value3);
+
+// , skip nested
+// const arrary = [["one", "two"], "three"];
+// let [[value1,], value3] = arrary;
+// console.log(value1, value3);    
+  
+console.log("hi");
---
+++ object.js
@@ -0,0 +1,47 @@
+// const obj = {
+//     name:"Corolla",
+//     model:2024,
+//     wheel:4,
+//     engine:true
+// }
+// let {name,model,wheel,engine}= obj
+// console.log(name,model,wheel,engine);
+
+
+// rename of key 
+// const obj = {
+//     name:"Corolla",
+//     model:2024,
+//     wheel:4,
+//     engine:true
+// }
+// let {name:myName,model,wheel,engine}= obj
+// console.log(myName,model,wheel,engine);      //name:myName
+// no skip destructing in the object 
+
+
+// // nested
+// const obj = {
+//     name: "Corolla",
+//     model: 2024,
+//     wheel: {
+//         num: 4,
+//         color: "Black"
+//     },
+//     engine: true
+// }
+// let { name, model, wheel:{num,color}, engine } = obj
+// console.log(num,color);
+// console.log({num,color});     // arrary of obj
+
+
+// const obj = {
+//     name:"madiha",
+//     cls:12,
+//     isLove:true
+// }
+
+// let {name:MyLove,cls,isLove}=obj;
+// console.log(MyLove);
+
+console.log("hello");
